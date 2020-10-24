eb287ac046bc629e08d2daa253456338e4fa6a09
iory <ab.ioryz@gmail.com>
2020-10-25 02:20:49 +0900

    Add resize for API limit

--- line_notify/__init__.py
+++ line_notify/__init__.py
@@ -1,10 +1,12 @@
 # flake8: noqa
 
+from __future__ import absolute_import
+
 import pkg_resources
 
 
 __version__ = pkg_resources.get_distribution("pyline-notify").version
 
 
-from .line_notify import LineNotify
-from .line_notify import send_line_notify
+from line_notify.line_notify import LineNotify
+from line_notify.line_notify import send_line_notify
